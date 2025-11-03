4a4ace8650f98aad9f85b1cb9f629e0503812757
ChrisCoolDev <talchrist09@gmail.com>
2025-11-03 18:55:17 +0300

    change de lemon squeezy reditection url after purchasing

--- api/lemon-checkout.js
+++ api/lemon-checkout.js
@@ -49,7 +49,8 @@ export default async function handler(req, res) {
               },
             },
             product_options: {
-              redirect_url: 'https://qalive.ink/?success=true',
+              redirect_url:
+                'qalive-app-git-develop-chriscooldevs-projects.vercel.app/?success=true',
             },
           },
           relationships: {
